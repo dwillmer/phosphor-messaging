f30de902f3ca115b73f2a8191521a427d9fe98e6
S. Chris Colbert <sccolbert@gmail.com>
2015-08-15 19:19:54 -0400

    update to dts-generate

--- scripts/dtsgenerate.js
+++ scripts/dtsgenerate.js
@@ -1,4 +1,8 @@
-require('dts-bundle').bundle({
+require('dts-generator').generate({
   name: 'phosphor-messaging',
-  main: 'lib/index.d.ts',
+  main: 'phosphor-messaging/index',
+  baseDir: 'lib',
+  files: ['index.d.ts'],
+  out: 'lib/phosphor-messaging.d.ts',
+  target: 1 // ts.ScriptTarget.ES5
 });
